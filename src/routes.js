7e41d10d3f0e3396e90931ee02df9edc55df2b8b
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-04-02 13:54:46 -0300

    Colocando elementos na page incidents

--- src/routes.js
+++ src/routes.js
@@ -10,9 +10,9 @@ const AppStack = createStackNavigator();
 export default function Routes() {
     return (
         <NavigationContainer >
-            <AppStack.Navigator screenOptions={{ headerShown: true }}>
+            <AppStack.Navigator screenOptions={{ headerShown: false }}>
                 <AppStack.Screen name='Incidents' component={Incidents} />
-                <AppStack.Screen name='"Detail"' component={Detail} />
+                <AppStack.Screen name='Detail' component={Detail} />
             </AppStack.Navigator>
         </NavigationContainer>
     );
