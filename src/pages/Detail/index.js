cf8da72ec74e5bc8b121b17d0656a2d720cc527f
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-04-04 15:34:10 -0300

    conectando sendEmail e send Whatsapp

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -2,39 +2,53 @@ import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
-import { View, FlatList, Text, Image, TouchableOpacity } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
+
+import { View, Linking, Text, Image, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
 import styles from './styles';
 
-export default function Detail(){
+export default function Detail() {
 
     const navigation = useNavigation();
+    const message = 'Olá APAD, estou entrando em contato pois gostaroa de ajudar no casso "Cadelinha atropelada" com o vaor de R$120,00 reais.';
 
-    function navigateBack(){
+    function navigateBack() {
         navigation.goBack();
     }
 
-    return(
+    function sendMail() {
+        MailComposer.composeAsync({
+            subject: 'Herói do caso: Cadelinha atropelada',
+            recipients: ['anisioaleixo@yahoo.com.br'],
+            body: message,
+        })
+    }
+    function sendWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=5591989649266&text=${message}`)
+    }
+
+    return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} />
-                <TouchableOpacity 
+                <TouchableOpacity
                     onPress={navigateBack}>
                     <Feather name="arrow-left" size={28} color="#e82041" />
                 </TouchableOpacity>
             </View>
-            
+
             <View style={styles.incident}>
-                    <Text style={[styles.incidentProperty,{marginTop:0}]}>ONG:</Text>
-                    <Text style={styles.incidentValue}>APAD</Text>
+                <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+                <Text style={styles.incidentValue}>APAD</Text>
 
-                    <Text style={styles.incidentProperty}>CASO:</Text>
-                    <Text style={styles.incidentValue}>Cadelinha atropelada.</Text>
+                <Text style={styles.incidentProperty}>CASO:</Text>
+                <Text style={styles.incidentValue}>Cadelinha atropelada.</Text>
 
-                    <Text style={styles.incidentProperty}>VALOR:</Text>
-                    <Text style={styles.incidentValue}>R$120,00</Text>
+                <Text style={styles.incidentProperty}>VALOR:</Text>
+                <Text style={styles.incidentValue}>R$120,00</Text>
 
             </View>
             <View style={styles.contactBox}>
@@ -42,24 +56,21 @@ export default function Detail(){
                 <Text style={styles.heroTitle}>Salve o dia!</Text>
                 <Text style={styles.heroTitle}>Seja o heroi deste caso.</Text>
                 <Text style={styles.heroDescription}>Entre em contato:</Text>
-                
+
                 <View style={styles.actions}>
-                    <TouchableOpacity 
-                        onPress={()=>{}}
+                    <TouchableOpacity
+                        onPress={sendWhatsapp}
                         style={styles.action}>
-                            <Text style={styles.actionText}>Whatsapp</Text>
+                        <Text style={styles.actionText}>Whatsapp</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity 
-                        onPress={()=>{}}
+                    <TouchableOpacity
+                        onPress={sendMail}
                         style={styles.action}>
-                            <Text style={styles.actionText}>E-mail</Text>
+                        <Text style={styles.actionText}>E-mail</Text>
                     </TouchableOpacity>
                 </View>
             </View>
-
-
-
         </View>
     );
 }
